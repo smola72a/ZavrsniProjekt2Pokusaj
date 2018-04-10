7f3fd82f810b367ddc3362a796db79a90364d9c9
DESKTOP-TF8668C\Unity 3D <>
2018-04-10 13:17:01 +0200

    no message

--- ZavrsniProject/Assets/Scripts/Inventory.cs
+++ ZavrsniProject/Assets/Scripts/Inventory.cs
@@ -6,16 +6,19 @@ public class Inventory : MonoBehaviour
 {
     public List<SOItem> ItemsInInventory = new List<SOItem>();
 
+	//TODO: treba li ti zaista uopće ova lista
     public List<Vector2> ItemsOnSlot = new List<Vector2>();
 
+	//TODO: jel ti ovo treba?
     public int StorageInInventory;
 
+	//TODO: možeš napravit bez ove dvije. NumOfItemsInInventory ti je ItemsInInventory.Count, a InventoryIndex ti ni ne treba tj možeš ga izvuć isto iz liste
     private int NumOfItemsInInventory;
     public int InventoryIndex;
 
     private void Awake()
     {
-        
+        //TODO: više nemaš razliku između weapon i item tak da stavi additemininventory
         LootGain.itemIsPickedUp.AddListener(AddWeaponInInventory);
     }
 
@@ -23,6 +26,7 @@ public class Inventory : MonoBehaviour
     {
         if (NumOfItemsInInventory < 6)
         {
+			//TODO: kuiš onda ti ni ovo ne treba
             Vector2 Vektor = new Vector2(NumOfItemsInInventory, InventoryIndex);
             ItemsOnSlot.Add(Vektor);
             ItemsInInventory. Add(ItemToAdd);
@@ -30,8 +34,10 @@ public class Inventory : MonoBehaviour
         }
     }
 
+	//TODO:ovo se može ovak radit al može se vjv i prek indexa, tak da vidi kaj ti je zgodnije. Možda će ti bit lakše pristupit itemu prek indexa, a možda prek itema.
     public void RemoveItemFromInventory (SOItem ItemToRemove)
     {
+		//TODO: treba vidjet kak se ponašaju ostali itemi u listi, tj dal se smanjuje ukupna veličina liste ili ostaje ista al je onda jedno mjesto prazno itd itd
         ItemsInInventory.Remove(ItemToRemove);
         NumOfItemsInInventory--;
     }
